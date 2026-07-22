581f6fb4481493bd1bef2b42387969f3615d5561
Babash95 <marziyabonu.n98@gmail.com>
2026-07-22 15:42:39 +0000

    Add business assistant selection

--- app/main.py
+++ app/main.py
@@ -1,47 +1,42 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi import Request
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="app/templates")
 
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+messages = []
 
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-  return templates.TemplateResponse(
-    request=request,
-    name="index.html"
-)
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={
+            "messages": messages
+        }
+    )
 
 
 @app.post("/chat", response_class=HTMLResponse)
 def chat(message: str = Form(...)):
 
-    answer = """
-    Спасибо за ваш вопрос.
+    messages.append({
+        "role": "user",
+        "text": message
+    })
 
-    Я AI-помощник для малого бизнеса.
-    Скоро я смогу помогать с клиентами,
-    продажами и документами.
-    """
+    messages.append({
+        "role": "ai",
+        "text": "Спасибо за вопрос. Я AI-помощник для малого бизнеса."
+    })
 
-    return f"""
-    <html>
-    <body>
-        <h1>🤖 AI Chat Business</h1>
-
-        <h3>Ваш вопрос:</h3>
-        <p>{message}</p>
-
-        <h3>Ответ AI:</h3>
-        <p>{answer}</p>
-
-        <a href="/">← Новый вопрос</a>
-    </body>
-    </html>
-    """
+    return HTMLResponse(
+        """
+        <script>
+        window.location.href="/";
+        </script>
+        """
+    )
